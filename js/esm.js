e34109c66832ea621bc060961ab39347c71c5357
Quentin Comte-Gaz <quentin@comte-gaz.com>
2017-08-24 13:17:41 +0200

    Ping: Fix minor issue

--- js/esm.js
+++ js/esm.js
@@ -248,7 +248,7 @@ esm.getPing = function() {
         {
             var html = '';
             html += '<tr>';
-            html += '<td><a href="'+data[line].host+'">'+data[line].host+'</a></td>';
+            html += '<td><a href="http://'+data[line].host+'" target="_blank">'+data[line].host+'</a></td>';
 
             html += '<td class="w15p"><span class="label ';
             if (data[line].ping.indexOf('Inf') > -1) {
